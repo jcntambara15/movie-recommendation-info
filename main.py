a07bd186305e0a9db22a6e09e933fb8cdb52c409
Jean Claude Ntambara <jc.ntambara@gmail.com>
2022-07-06 21:54:34 +0000

    Added movie list function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import pandas as pd
 import sqlalchemy as db	
 import json
+import pprint
 
 session_id= 'c72032122d67260cc02034445c4206ba96a84f0e'
 list_id = '8209394'
@@ -27,3 +28,29 @@ print(responseL.json())
 def database():
     # https://developers.themoviedb.org/3/movies/get-movie-recommendations
  sample = requests.get('https://api.themoviedb.org/3/movie/438148/recommendations?api_key=dd1c306527d8caa33b2acb40c88ce2ae&language=en-US&page=1').json();
+ return sample
+
+# pprint.pprint(database())
+def get_movie_list(response):
+    """Creating a list of movies"""
+    list_movies = []
+    for dict_item in response:
+        list_movies.append(dict_item['title'])
+    return list_movies
+
+def create_database(response):
+    """Getting a movie from each dictionary and add it to the database"""
+    my_data_frame = pd.DataFrame(
+                get_movie_list(response), 
+                columns=['movies']
+                )
+    engine = db.create_engine('sqlite:///movies.db')
+    my_data_frame.to_sql('data', con=engine, if_exists='replace', index=False)
+    query_result = engine.execute("SELECT * FROM data;").fetchall()
+    return pd.DataFrame(query_result)
+
+
+movies = database()['results']
+print(json.dumps(movies, indent=4))
+create_database(movies)
+print(get_movie_list(movies))
